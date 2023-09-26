71c8467cae2e12a600feb914f4e412fdefae4f7b
duoluodexiaoxiaoyuan <1203125214@qq.com>
2023-09-26 13:47:34 +0800

    feature: 提交代码

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,40 +8,20 @@ export default function Home() {
 
   return (
     <>
-     {/*pc start */}
-      {/* 通过看效果发现padding的间距是随着像素而变化的 */}
-       {/* <main className="h-screen">
-          <div className="m-12 bg-green-400 h-2/3 flex justify-center items-center text-3xl md:p-14 lg:p-16 xl:p-20">
-            <p className="bg-sky-400">nihaoshijie</p>  
-          </div>
-       </main> */}
-       {/* end */}
-       {/* start 响应式布局先写移动端 md768的时候开始改变 */}
-        {/* <main className="grid gap-8 md:grid-cols-2 md:max-2xl:bg-blue-400 sm:text-left">
-          <div>
-            <img className="w-full rounded-lg" src="https://tse3-mm.cn.bing.net/th/id/OIP-C.ZkoPhpKfJwsvGmpm8RsragHaFp?pid=ImgDet&rs=1" alt="" />
-          </div>
-          <div>
-            <h1 className="text-4xl font-medium mb-2">Headline</h1>
-            <p className="md:tablet:text-red-400">牛倒江九老破随默否图！富奇停死摇森级存？跳烟睡各话发青街的临双岁些之跳希罪？围找宝样全。打红读周弄睡风贝痛渐美且接抱雷睡。奶脱别深社桌饭入敢从！基品刻小司初致真痛觉童求景及！钟不若千写车语游单间助围了局喝欧或三！录求如渐烟火妻贝挥戴乐纸用录小？智段呀存首卖泪哪制到挥听必定续香疑意！岁带相工智牛！义抱号学忽天神自汉默被以父右商带？成头佛速娜破火举生着？消船杰坦尔勒拥礼凡它树光上波。爸望摇成毫治园穿？京睛警集短毛江贝便座三很局定显创牌！它？</p>
-          </div>
-        </main> */}
-        {/* end */}
+      {/* 现在的问题是页面内容过多，假如你把最外层的div设置为铺满屏幕，当你里面的内容超出屏幕的时候，里面的div宽度没有
+          充满盒子，所以就会看到大片空白，这个时候应该如何解决呢
+      */}
+      {/* start 第一种方法，最外层设置最小高度 */}
+      {/* <div className="min-h-screen bg-purple-400">
+          <div className="h-[2000px] bg-red-600 w-[400px]"></div>
+      </div> */}
+      {/* end */}
 
-        {/* 看下不变是什么效果 */}
-        {/* md最小768    sm640 - md768 - lg1024 -xl1280 - 2xl1536 */}
-        {/* md最小640生效，就是满足最小宽度生效 */}
-        {/* max-md最大640 0-640生效， 比喻成买东西就理解了 */}
-        <main className="grid gap-8 md:grid-cols-2 md:max-2xl:bg-blue-400 sm:text-left">
-          <div>
-            <img src="/user-onboarding-left.png"/>
-            {/* <img className="w-full rounded-lg" src="https://tse3-mm.cn.bing.net/th/id/OIP-C.ZkoPhpKfJwsvGmpm8RsragHaFp?pid=ImgDet&rs=1" alt="" /> */}
-          </div>
-          <div>
-            <h1 className="text-4xl font-medium mb-2">Headline</h1>
-            <p className="md:tablet:text-red-400">牛倒江九老破随默否图！富奇停死摇森级存？跳烟睡各话发青街的临双岁些之跳希罪？围找宝样全。打红读周弄睡风贝痛渐美且接抱雷睡。奶脱别深社桌饭入敢从！基品刻小司初致真痛觉童求景及！钟不若千写车语游单间助围了局喝欧或三！录求如渐烟火妻贝挥戴乐纸用录小？智段呀存首卖泪哪制到挥听必定续香疑意！岁带相工智牛！义抱号学忽天神自汉默被以父右商带？成头佛速娜破火举生着？消船杰坦尔勒拥礼凡它树光上波。爸望摇成毫治园穿？京睛警集短毛江贝便座三很局定显创牌！它？</p>
-          </div>
-        </main>
+      {/* start 第一种方法，最外层设置最小高度 */}
+      <div className="min-h-screen bg-purple-400">
+          <div className="h-[2000px] bg-red-600 w-[400px]"></div>
+      </div>
+      {/* end */}
     </>
   )
 }
